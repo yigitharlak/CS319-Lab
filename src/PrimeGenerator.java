bcede46b320e72b91b30a0c1be3b4ce6fd960dc1
Name Surname <email@ug.bilkent.edu.tr>
2020-10-08 13:20:37 +0300

    init project

---
+++ src/PrimeGenerator.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class PrimeGenerator {
+
+    private boolean isPrime(int number) {
+        if (number <= 1)
+            return false;
+
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
