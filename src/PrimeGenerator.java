3fe514fe4e703599dde340171e7bc4f170781143
Name Surname <email@ug.bilkent.edu.tr>
2020-10-08 13:22:55 +0300

    fix the bug

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -6,7 +6,7 @@ public class PrimeGenerator {
         if (number <= 1)
             return false;
 
-        for (int i = 2; i <= number; i++) {
+        for (int i = 2; i < number; i++) {
             if (number % i == 0)
                 return false;
         }
