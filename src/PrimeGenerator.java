a0881b0a95671e38345b5afda0cd64ea8c5e2e9d
Mehmet Yiğit Harlak <yigit.harlak@ug.bilkent.edu.tr>
2020-10-24 13:45:33 +0300

    add generatePrimes() method.

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -2,16 +2,26 @@ import java.util.ArrayList;
 
 public class PrimeGenerator {
 
-    private boolean isPrime(int number) {
-        if (number <= 1)
-            return false;
 
-
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0)
-                return false;
-        }
-
-        return true;
+  public​ ArrayList<Integer> ​generatePrimes​(​int​ limit) {
+    ArrayList<Integer> primes = ​new​ ArrayList<Integer>();
+    ​for​ (​int​ i = ​1​; i <= limit; i++) {
+      ​if​ (isPrime(i))
+      primes.​add​(i);
     }
+    ​   return​ primes;
+  }
+
+  private boolean isPrime(int number) {
+    if (number <= 1)
+    return false;
+
+
+    for (int i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i == 0)
+      return false;
+    }
+
+    return true;
+  }
 }
